db265e28f4fd0b3949502107c9b2eaee37775668
Andrey Vokov <ballgameandrew@yandex.ru>
2022-03-31 02:48:45 +0300

    Added some fixes in gradle (again) to help in executing UI tests

--- app/src/androidTest/kotlin/com/kabouzeid/gramophone/ui/activities/MainActivityTest.kt
+++ app/src/androidTest/kotlin/com/kabouzeid/gramophone/ui/activities/MainActivityTest.kt
@@ -1,36 +1,50 @@
 package com.kabouzeid.gramophone.ui.activities
 
-import android.R
-import androidx.test.InstrumentationRegistry
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import com.kabouzeid.gramophone.ui.activities.MainActivity
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class MainActivityTest {
-    @Rule
+    @get:Rule
+    // yes I know, ActivityTestRule deprecated and it's better to use ActivityScenarioRule
+    // but this new google "solution" doesn't work with vm and my physical phone.
+    // java.lang.AssertionError: Activity never becomes requested state "[DESTROYED, CREATED, STARTED, RESUMED]" (last lifecycle transition = "PRE_ON_CREATE")
+    // solutions that was on StackOverflow, doesn't helped
     var activityActivityTestRule = ActivityTestRule(
         MainActivity::class.java
     )
 
-    @Test
-    fun useAppContext() {
-        val context = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.kabouzeid.gramophone.ui.activities", context.packageName)
+    @Test fun useAppContext() {
+        /*val scenario: ActivityScenario<*> = activityActivityTestRule.scenario
+        scenario.moveToState(Lifecycle.State.RESUMED);    // Moves the activity state to State.RESUMED.
+        scenario.moveToState(Lifecycle.State.STARTED);    // Moves the activity state to State.STARTED.
+        scenario.moveToState(Lifecycle.State.CREATED);    // Moves the activity state to State.CREATED.
+        scenario.moveToState(Lifecycle.State.DESTROYED);*/
+        Espresso.onView(withText("Hello world!")).check(matches(isDisplayed()))
+        /*val context = InstrumentationRegistry.getInstrumentation().context
+        assertEquals("com.kabouzeid.gramophone.debug.test", context.packageName)*/
     }
 
-    @Test
+    /*@Test
     fun clickButtonHome() {
-        Espresso.onView(withId(R.id.navigation_home))
-            .perform(click()).check(matches(ViewMatchers.isDisplayed()))
-    }
+
+        //NavigationViewActions.navigateTo(R.id.nav_folders)
+        //Espresso.onView(ViewMatchers.withText("Folders")).perform(click())
+    }*/
 }
