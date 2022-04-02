6e3b9481d39c8c4537e2a4d1010fdafd8ff0a5c3
Andrey Vokov <ballgameandrew@yandex.ru>
2022-04-02 03:03:06 +0300

    Realized Download of mp3 files, for player testing

--- app/src/androidTest/kotlin/com/kabouzeid/gramophone/ui/activities/MainActivityTest.kt
+++ app/src/androidTest/kotlin/com/kabouzeid/gramophone/ui/activities/MainActivityTest.kt
@@ -1,21 +1,26 @@
 package com.kabouzeid.gramophone.ui.activities
 
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.isOpen
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import org.junit.Assert.assertEquals
+import com.kabouzeid.gramophone.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.File
 
 
 @RunWith(AndroidJUnit4::class)
@@ -30,21 +35,49 @@ class MainActivityTest {
         MainActivity::class.java
     )
 
-    @Test fun useAppContext() {
+    @Test
+    fun useAppContext() {
         /*val scenario: ActivityScenario<*> = activityActivityTestRule.scenario
         scenario.moveToState(Lifecycle.State.RESUMED);    // Moves the activity state to State.RESUMED.
         scenario.moveToState(Lifecycle.State.STARTED);    // Moves the activity state to State.STARTED.
         scenario.moveToState(Lifecycle.State.CREATED);    // Moves the activity state to State.CREATED.
         scenario.moveToState(Lifecycle.State.DESTROYED);*/
-        Espresso.onView(withText("Hello world!")).check(matches(isDisplayed()))
+        //Espresso.onView(withText("Hello world!")).check(matches(isDisplayed()))
         /*val context = InstrumentationRegistry.getInstrumentation().context
         assertEquals("com.kabouzeid.gramophone.debug.test", context.packageName)*/
     }
 
-    /*@Test
+    @Test
     fun clickButtonHome() {
+        NavigationViewActions.navigateTo(R.id.nav_folders)
+        val manager =
+            activityActivityTestRule.activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val uri: Uri =
+            Uri.parse("https://cdn.pixabay.com/download/audio/2022/03/23/audio_07b2a04be3.mp3?filename=order-99518.mp3")
+        val request =
+            DownloadManager.Request(uri).setTitle("music.mp3").setDescription("Downloading...")
+                .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI xor DownloadManager.Request.NETWORK_MOBILE)
+                .setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_MUSIC,
+                "downloadfileName"
+            ).setMimeType("*/*")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setAllowedOverMetered(true)
+        val file = File("/root/storage/emulated/0/music")
+        manager.enqueue(request)
+        /*Espresso.onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        Espresso.onView(withId(R.id.drawer_layout)).check(matches(isOpen()))
+//        Espresso.onView(withText("Phonograph")).check(matches(isDisplayed()))
+        Espresso.onView(withId(R.id.navigation_view)).perform(NavigationViewActions.navigateTo(R.id.nav_library))
+        Espresso.onView(withText("ALBUMS")).check(matches(isDisplayed()))*/
 
-        //NavigationViewActions.navigateTo(R.id.nav_folders)
-        //Espresso.onView(ViewMatchers.withText("Folders")).perform(click())
-    }*/
+        Espresso.onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        Espresso.onView(withId(R.id.drawer_layout)).check(matches(isOpen()))
+        Espresso.onView(withId(R.id.navigation_view))
+            .perform(NavigationViewActions.navigateTo(R.id.nav_folders))
+        Espresso.onView(withText("MUSIC")).check(matches(isDisplayed()))
+        /*Espresso.onView(withText("Folders")).check(matches(isDisplayed()))
+        Espresso.onView(withText("ROOT")).check(matches(isDisplayed()))*/
+
+    }
 }
---
+++ app/src/androidTest/kotlin/com/kabouzeid/gramophone/ui/fragments/mainactivity/folders/FoldersFragmentTest.java
@@ -0,0 +1,7 @@
+package com.kabouzeid.gramophone.ui.fragments.mainactivity.folders;
+
+import static org.junit.Assert.*;
+
+public class FoldersFragmentTest {
+
+}
